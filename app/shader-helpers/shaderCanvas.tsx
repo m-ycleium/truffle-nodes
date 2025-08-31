6e1ceb0d83d12bb322f26b4e76104a444a3bbcc4
M <vault641111@gmail.com>
2025-08-31 15:31:33 -0700

    shader canvas working

--- app/shader-helpers/shaderCanvas.tsx
+++ app/shader-helpers/shaderCanvas.tsx
@@ -1,6 +1,19 @@
 "use client";
 
 import { useRef, useEffect, useCallback } from "react";
+import * as THREE from "three";
+
+const isWebGLAvailable = () => {
+  try {
+    const canvas = document.createElement("canvas");
+    return !!(
+      window.WebGLRenderingContext &&
+      (canvas.getContext("webgl") || canvas.getContext("experimental-webgl"))
+    );
+  } catch (e) {
+    return false;
+  }
+};
 
 function ShaderCanvas() {
   const canvasRefs = useRef<HTMLCanvasElement | null>(null);
@@ -11,16 +24,51 @@ function ShaderCanvas() {
     const canvas = canvasRefs.current;
     if (!canvas) return;
 
-    const gl = canvas.getContext("webgl");
-    if (!gl) {
+    if (!isWebGLAvailable()) {
       console.error("WebGL not supported");
-    } else {
-      console.log(gl);
-      console.log;
+      return;
     }
+
+    const dpr = Math.min(2, window.devicePixelRatio || 1);
+
+    const renderer = new THREE.WebGLRenderer({ canvas, antialias: false });
+    renderer.setPixelRatio(dpr);
+    renderer.setSize(500, 500);
+    const resolution = new THREE.Vector2(
+      renderer.domElement.width,
+      renderer.domElement.height
+    );
+
+    const scene = new THREE.Scene();
+    const camera = new THREE.OrthographicCamera(-1, 1, 1, 1, 0, 1);
+    const geometry = new THREE.PlaneGeometry(2, 2);
+
+    const material = new THREE.ShaderMaterial({
+      uniforms: {
+        iResolution: { value: resolution },
+        iTime: { value: 0 },
+      },
+      vertexShader: `
+        void main() {
+          gl_Position = vec4(position, 1.0);
+        }
+      `,
+    });
+
+    const mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+
+    // todo: autoscale canvas
+    const setSize = () => {
+      renderer.setPixelRatio(dpr);
+      renderer.setSize(500, 500);
+      material.uniforms.iResolution.value.set();
+    };
 
     const animate = (timestamp: DOMHighResTimeStamp) => {
       animationFrameId.current = requestAnimationFrame(animate);
+      material.uniforms.iTime.value = timestamp;
+      renderer.render(scene, camera);
     };
 
     animationFrameId.current = requestAnimationFrame(animate);
