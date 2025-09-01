b26384cef2e0355e1e2c31d5d2aaa026483c7a9c
M <vault641111@gmail.com>
2025-08-31 19:59:06 -0700

    adjusted constansts

--- app/shader-helpers/fragment.ts
+++ app/shader-helpers/fragment.ts
@@ -3,7 +3,7 @@
 export default `
 uniform vec2 iResolution;
 uniform float iTime;
-#define OCTAVES 4
+#define OCTAVES 5
 
 float rand(vec2 p) {
   return fract(sin(dot(p, vec2(11., 32.))) * 11111.);
@@ -23,10 +23,11 @@ float noise (vec2 p) {
 float fbm(vec2 p) {
   float dampenFactor = 0.5;
   float range = 15.0;
-  float f = 0.0, a = 1.0;
+  float f = 0.0, a = 0.5;
+  float scale = 2.;
   for (int i = 0; i < OCTAVES; i++) {
     f += a*noise(p);
-    p = 2.0 * p + range;
+    p = scale * p + range;
     a *= dampenFactor;
   }
   return f;
@@ -42,8 +43,11 @@ void main() {
   
   vec2 p = uv;
 
-  vec2 flow = vec2(fbm(p + iTime * 0.1), fbm(p - iTime * 0.1)); 
-  float smoke = fbm(flow + iTime * 0.1);
+  vec2 flow = vec2(fbm(p*1.5 + 0.10*iTime), fbm(p*1.5 - 0.11*iTime));
+  flow   = p + 0.7*flow;    
+  float smoke = fbm(flow * 2. + iTime * 0.1);
+
+  smoke = pow(smoke, 1.2);
 
   fragColor = vec4(vec3(smoke), 1.0);
   gl_FragColor = fragColor;
